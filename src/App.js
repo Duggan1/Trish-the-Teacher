0a83b2837ffecf9abb1a09fe4b00213c197af5de
Kevin Duggan <kevinduggan95@gmail.com>
2023-07-18 17:49:25 -0500

    lift off

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>
-          Welcome to </h2><h1>Trish the Teacher !
+          Welcome to </h2><h1  style={{color:'yellow', marginTop:'-2%', marginBottom:'0%'}}>Trish the Teacher !
         </h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <div style={{ height: '200px',}}className="teach"></div>
         
-        <p>Website currently under construction </p>
+        <p>Website currently under construction </p><p style={{color:'yellow'}}>Sorry! </p>
+        <img src={logo} className="App-logo" alt="logo" /><p style={{color:'yellow'}}>Come back soon! </p><div style={{ height: '200px',}}className="apple"></div>
+        
+        
       </header>
     </div>
   );
